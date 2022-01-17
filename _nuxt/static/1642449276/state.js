b8cfacf19b92b6481f4f04b476f9ca11e0e2f81e
Jose Manuel Perez <manuelperez9a@gmail.com>
2022-01-17 13:55:32 -0600

    9513b76

---
+++ _nuxt/static/1642449276/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002Fwedding-myc\u002F_nuxt\u002Fstatic\u002F1642449276",layout:"default",error:a,state:{menuIsOpen:b,footerIsOpen:b},serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fwedding-myc\u002F",assetsPath:"\u002Fwedding-myc\u002F_nuxt\u002F",cdnURL:a}}}}(null,false));
